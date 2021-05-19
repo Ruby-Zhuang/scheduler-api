1ab9ab361fab3eff5be0701a96c3b286029bb5d7
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-19 17:25:28 -0400

    Reconfigure ssl for heroku

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,13 @@
-const pg = require("pg");
+const pg = require('pg');
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || "",
+  connectionString: process.env.DATABASE_URL || '',
   // For extra protection
-  ssl: process.env.DATABASE_URL ? true : false
+  ssl: process.env.DATABASE_URL
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
   // ssl: {
   //   rejectUnauthorized: false,
   // },
@@ -11,6 +15,6 @@ const client = new pg.Client({
 // console.log(process.env.DATABASE_URL);
 client
   .connect()
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+  .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
 
 module.exports = client;
