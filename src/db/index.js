c9e31976d18fd60edd27fd4f2975d59546ca0114
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-19 09:40:28 -0400

    Change ssl in database index.js to have it work on Mac M1

--- src/db/index.js
+++ src/db/index.js
@@ -2,11 +2,13 @@ const pg = require("pg");
 
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL || "",
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  // For extra protection
+  ssl: process.env.DATABASE_URL ? true : false
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
 });
-
+// console.log(process.env.DATABASE_URL);
 client
   .connect()
   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
